d9cf7f3d8b9e2928386f168b33049ba9c2cb0b5c
s-ferrer <saraferrer@gmail.com>
2022-07-06 12:03:48 +0200

    Move components folder in src folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import dataCard from "./data";
 
-function App() {
+export default function App() {
+  const cards = dataCard.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Card />
+      <section className="card-list">{cards}</section>
     </div>
   );
 }
-
-export default App;
